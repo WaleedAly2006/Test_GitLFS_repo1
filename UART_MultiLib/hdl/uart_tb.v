92aa20d17d0fe44fc145c021e283dd8bb9b85bca
WekaAly <113982518+WekaAly@users.noreply.github.com>
2022-09-20 23:30:04 +0100

    weka: modify uart_tb

--- UART_MultiLib/hdl/uart_tb.v
+++ UART_MultiLib/hdl/uart_tb.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fe04328b11ca33be68157c4ffb4cb4d440ef8fa28577aea231da4e324a88464
-size 1200
+oid sha256:814b3bde9aa216cac3e55b7033d91c581a3408bb672bc7a73d10bb59034417c4
+size 1146
